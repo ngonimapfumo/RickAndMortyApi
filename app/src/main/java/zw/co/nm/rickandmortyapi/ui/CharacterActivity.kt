6ebacf0bda894ebac7a1fe850a52ab3fcf7dd049
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2023-03-30 01:43:15 +0200

    UI fixes

--- app/src/main/java/zw/co/nm/rickandmortyapi/ui/CharacterActivity.kt
+++ app/src/main/java/zw/co/nm/rickandmortyapi/ui/CharacterActivity.kt
@@ -2,10 +2,12 @@ package zw.co.nm.rickandmortyapi.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.launch
+import zw.co.nm.rickandmortyapi.R
 import zw.co.nm.rickandmortyapi.databinding.ActivityCharacterBinding
 import zw.co.nm.rickandmortyapi.models.responses.GetSingleCharacterResponse
 import zw.co.nm.rickandmortyapi.viewmodels.GetCharacterViewModel
@@ -19,14 +21,33 @@ class CharacterActivity : AppCompatActivity() {
         val getCharacterViewModel = ViewModelProvider(this)[GetCharacterViewModel::class.java]
 
         lifecycleScope.launch {
-            getCharacterViewModel.getCharacter(93).collect(::response)
+            //generating random characters for testing here
+            getCharacterViewModel.getCharacter((1..160).random()).collect(::response)
         }
 
     }
 
     private fun response(res: GetSingleCharacterResponse?) {
-        activityCharacterBinding.nameTextView.text = res?.name
-        activityCharacterBinding.specieTxt.text = res?.species
-        Picasso.get().load(res?.image).into(activityCharacterBinding.characterImg)
+        if (res?.status!! == "Alive") {
+            activityCharacterBinding.statusTxt.setTextColor(
+                ContextCompat.getColor(
+                    this@CharacterActivity,
+                    R.color.green
+                )
+            )
+        } else {
+            activityCharacterBinding.statusTxt.setTextColor(
+                ContextCompat.getColor(
+                    this@CharacterActivity,
+                    R.color.red
+                )
+            )
+        }
+
+        activityCharacterBinding.nameTextView.text = res.name
+        activityCharacterBinding.specieTxt.text = res.species
+        activityCharacterBinding.locationTxt.text = res.location.name
+        activityCharacterBinding.statusTxt.text = res.status
+        Picasso.get().load(res.image).into(activityCharacterBinding.characterImg)
     }
 }
